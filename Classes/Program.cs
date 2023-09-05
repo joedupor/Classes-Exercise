74f2247905f14b854172e079a2c557c9232224f9
joedupor <joedupor@gmail.com>
2023-09-05 14:30:59 -0500

    init commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Classes
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car carOne = new Car("Jeep", "Cherokee", 2001);           
+            Console.WriteLine($"Your car make is {carOne.Make}, the model is {carOne.Model}, and the year is {carOne.Year}!");
+                
         }
-    }
+    } 
 }
